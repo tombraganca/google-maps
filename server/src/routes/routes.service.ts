75a9a823f3e1abd2b54b8b568860f1954d2abbab
Gleydiston Bragança <gleydiston.santos@actdigital.com>
2023-09-26 01:05:53 -0300

    start project

---
+++ server/src/routes/routes.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@nestjs/common';
+import { CreateRouteDto } from './dto/create-route.dto';
+import { UpdateRouteDto } from './dto/update-route.dto';
+import { PrismaService } from 'src/prisma/prisma/prisma.service';
+
+@Injectable()
+export class RoutesService {
+
+  constructor(private prismaService: PrismaService) {}
+
+  create(createRouteDto: CreateRouteDto) {
+    return this.prismaService.route.create({
+      data: {
+        name: createRouteDto.name,
+        source:{
+          name: `origem`,
+          location: {
+            lat: 0,
+            lng: 0
+          }
+        },
+        destination:{
+          name: `destino`,
+          location: {
+            lat: 0,
+            lng: 0
+          }
+        },
+        distance: 0,
+        duration: 0,
+        directions: `{}`
+      }
+    })
+  }
+
+  findAll() {
+    return this.prismaService.route.findMany();
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} route`;
+  }
+
+  update(id: number, updateRouteDto: UpdateRouteDto) {
+    return `This action updates a #${id} route`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} route`;
+  }
+}
